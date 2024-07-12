534eb97e2be715f8e6b39a8fae34fc18361cd0d0
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-07-12 22:20:21 +0600

    Implement api for create course

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 require('dotenv').config();
 import { ErrorMiddleware } from './middleware/error';
 import userRouter from './routes/user.route';
+import courseRouter from './routes/course.route';
 
 // body parser
 app.use(express.json({limit: '50mb'}));
@@ -19,7 +20,7 @@ app.use(cors({
 
 // Routes
 app.use('/api/v1', userRouter);
-
+app.use('/api/v1', courseRouter);
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
