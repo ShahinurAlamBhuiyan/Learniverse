6f164965558d1fe372ba637a12b466578799defe
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-07-13 10:02:42 +0600

    Implement api for get single course without purchased

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { editCourse, uploadCourse } from '../controllers/course.controller';
+import { editCourse, getSingleCourse, uploadCourse } from '../controllers/course.controller';
 import { authorizeRoles, isAuthenticated } from '../middleware/auth';
 const courseRouter = express.Router();
 
@@ -7,6 +7,8 @@ const courseRouter = express.Router();
 courseRouter.post("/create-course", isAuthenticated, authorizeRoles("admin"), uploadCourse);
 
 courseRouter.put("/edit-course/:id", isAuthenticated, authorizeRoles("admin"), editCourse);
+
+courseRouter.get("/get-course/:id", getSingleCourse);
 
 
 
