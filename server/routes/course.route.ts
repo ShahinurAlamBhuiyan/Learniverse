dd8fc1bb59efb930e387ce96a7ea1f0e3a250604
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-07-27 21:22:45 +0600

    Implement add question api

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from '../controllers/course.controller';
+import { addQuestion, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from '../controllers/course.controller';
 import { authorizeRoles, isAuthenticated } from '../middleware/auth';
 const courseRouter = express.Router();
 
@@ -13,6 +13,8 @@ courseRouter.get("/get-course/:id", getSingleCourse);
 courseRouter.get("/get-courses", getAllCourses);
 
 courseRouter.get("/get-course-content/:id",isAuthenticated, getCourseByUser);
+
+courseRouter.put("/add-question",isAuthenticated, addQuestion);
 
 
 
