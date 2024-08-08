baacb02f027ce30d0bb53ab793124c2e810d3fba
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-08-08 12:52:35 +0600

    Implement user analytics

---
+++ server/controllers/analytics.controller.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import ErrorHandler from "../utils/ErrorHandler";
+import { generateLast12MonthData } from "../utils/analytics.generator";
+import userModel from "../models/user.model";
+
+// get users analytics --admin
+export const getUserAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const users = await generateLast12MonthData(userModel);
+
+      res.status(200).json({
+        success: true,
+        users,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
