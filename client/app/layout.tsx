f52443e1dbade297c780dc20437e7b4d388a6060
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-08-11 18:54:04 +0600

    [Theme] implemented

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
-import ThemeProvider from "@/utils/theme-provider";
+import ThemeProvider from "@/app/utils/theme-provider";
 
 
 const poppins = Poppins({
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} ${josefin.variable} !bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}>
+      <body className={`${poppins.variable} ${josefin.variable} !bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,24 @@
 'use client'
-import Heading from '@/utils/Heading'
+import Heading from '@/app/utils/Heading'
 import React, { FC, useState } from 'react'
+import Header from './components/Header'
 
 interface Props { }
 
 const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
   return (
     <div>
       <Heading
         title="Learniverse"
         description="Learniverse is a platform for students to learn and get help from teachers."
         keywords='Programming, MERN, Redux, Development, Machine Learning'
+      />
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
       />
     </div>
   )
