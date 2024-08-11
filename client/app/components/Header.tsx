3d85368ebcf36d356b805dcc0c7af9403c1465a2
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-08-11 19:29:58 +0600

    [Implement] dark & light nav

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React, { FC, useState } from 'react'
 import NavItem from '../utils/NavItem';
+import ThemeSwitcher from '../utils/ThemeSwitcher';
 
 type Props = {
     open: boolean;
@@ -8,7 +9,7 @@ type Props = {
     activeItem: number;
 }
 
-const Header: FC<Props> = ({activeItem}) => {
+const Header: FC<Props> = ({ activeItem }) => {
     const [active, setActive] = useState(false);
     const [openSidebar, setOpenSidebar] = useState(false);
 
@@ -23,7 +24,10 @@ const Header: FC<Props> = ({activeItem}) => {
     }
     return (
         <div className='w-full relative'>
-            <div className={`${active ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500" : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"}`}>
+            <div
+                className={`${active ?
+                    "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
+                    : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"}`}>
 
                 <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
                     <div className="w-full h-[80px] flex items-center justify-between p-3">
@@ -40,6 +44,7 @@ const Header: FC<Props> = ({activeItem}) => {
                                 activeItem={activeItem}
                                 isMobile={false}
                             />
+                            <ThemeSwitcher />
                         </div>
                     </div>
                 </div>
