27fd3093a2521363a213f89903240651d7bea48b
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-11-24 20:38:47 +0600

    registration action integrated

---
+++ client/redux/features/auth/authSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  token: "",
+  user: "",
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    userRegistration: (state, action) => {
+      state.token = action.payload.token;
+    },
+    userLoggedIn: (state, action) => {
+      state.token = action.payload.accessToken;
+      state.user = action.payload.user;
+    },
+    userLogOut: (state, action) => {
+      state.token = "";
+      state.user = "";
+    },
+  },
+});
+
+export const { userRegistration, userLoggedIn, userLogOut } = authSlice.actions;
