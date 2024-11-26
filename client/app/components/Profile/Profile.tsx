c4a64cee81d01a4ffb40c5eaf5970146f04825a6
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-11-26 21:54:34 +0600

    Add dynamic meta title for profile page

---
+++ client/app/components/Profile/Profile.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react"
+
+type Props = {}
+const Profile: FC<Props> = (props) => {
+    return (
+        <div>Profile</div>
+    )
+}
+export default Profile
--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -3,17 +3,21 @@ import { FC, useState } from "react"
 import Protected from "../hooks/useProtected"
 import Heading from "../utils/Heading"
 import Header from "../components/Header"
+import Profile from '../components/Profile/Profile'
+import { useSelector } from "react-redux"
 
 type Props = {}
 const page: FC<Props> = (props) => {
     const [open, setOpen] = useState(false);
     const [activeItem, setActiveItem] = useState(0);
     const [route, setRoute] = useState("Login");
+    const { user } = useSelector((state: any) => state.auth);
+
     return (
         <div>
             <Protected>
                 <Heading
-                    title="Learniverse"
+                    title={`${user.name}'s profile`}
                     description="Learniverse is a platform for students to learn and get help from teachers."
                     keywords='Programming, MERN, Redux, Development, Machine Learning'
                 />
@@ -24,7 +28,7 @@ const page: FC<Props> = (props) => {
                     route={route}
                     setRoute={setRoute}
                 />
-
+                <Profile />
             </Protected>
         </div>
     )
