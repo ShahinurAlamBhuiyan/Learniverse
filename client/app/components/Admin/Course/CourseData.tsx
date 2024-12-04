eb331d88533f304e8d732c5f6e31a48a67d6d105
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-12-04 21:57:28 +0600

    Course Prerequisites UI done for create course

--- client/app/components/Admin/Course/CourseData.tsx
+++ client/app/components/Admin/Course/CourseData.tsx
@@ -11,14 +11,25 @@ type Props = {
     setActive: (active: number) => void;
 }
 const CourseData: FC<Props> = ({ benefits, setBenefits, prerequisites, setPrerequisites, active, setActive }) => {
+
+    // Benefits
     const handleBenefitChange = (index: number, value: any) => {
         const updatedBenefits = [...benefits];
         updatedBenefits[index].title = value;
         setBenefits(updatedBenefits);
     }
-
     const handleAddBenefit = () => {
         setBenefits([...benefits, { title: "" }]);
+    }
+
+    // Prerequisites
+    const handlePrerequisiteChange = (index: number, value: any) => {
+        const updatedPrerequisites = [...prerequisites];
+        updatedPrerequisites[index].title = value;
+        setPrerequisites(updatedPrerequisites);
+    }
+    const handleAddPrerequisite = () => {
+        setPrerequisites([...prerequisites, { title: "" }]);
     }
     return (
         <div className="w-[80%] m-auto mt-24 block">
@@ -45,6 +56,33 @@ const CourseData: FC<Props> = ({ benefits, setBenefits, prerequisites, setPrereq
                 <AddCircleIcon
                     style={{ margin: "10px 0px", cursor: "pointer", width: "30px" }}
                     onClick={handleAddBenefit}
+                    className="dark:text-white text-black"
+                />
+            </div>
+            <div>
+                <label className={`${styles.label} text-[20px]`}>
+                    What are the prerequisites for students in this course?
+                </label>
+                <br />
+                {
+                    prerequisites.map((prerequisite: any, index: number) => (
+                        <input
+                            className={`${styles.input} my-2`}
+                            type="text"
+                            key={index}
+                            name="Prerequisite"
+                            required
+                            value={prerequisite.title}
+                            placeholder="You need basic knowledge of MERN stack"
+                            onChange={(e: any) => handlePrerequisiteChange(index, e.target.value)}
+
+                        />
+                    ))
+                }
+                <AddCircleIcon
+                    style={{ margin: "10px 0px", cursor: "pointer", width: "30px" }}
+                    onClick={handleAddPrerequisite}
+                    className="dark:text-white text-black"
                 />
             </div>
         </div>
