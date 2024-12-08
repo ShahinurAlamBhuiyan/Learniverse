c6bb17cf0897533c56889a8da1ce388272b0a944
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-12-09 01:41:44 +0600

    Complete full functionality for Course Video Player

--- client/app/utils/CoursePlayer.tsx
+++ client/app/utils/CoursePlayer.tsx
@@ -1,12 +1,42 @@
-import { FC } from "react";
+'use client'
+import { FC, useEffect, useState } from "react";
+import axios from 'axios';
 
 type Props = {
     videoUrl: string;
     title: string;
 }
 const CoursePlayer: FC<Props> = ({ videoUrl, title }) => {
+    const [videoData, setVideoData] = useState({
+        otp: "",
+        playbackInfo: ""
+    })
+
+    useEffect(() => {
+        axios.post(`${process.env.NEXT_PUBLIC_SERVER_URI}/get-video-cipher-otp`, {
+            videoId: videoUrl
+        }).then((res) => {
+            setVideoData(res.data)
+        })
+    }, [videoUrl])
     return (
-        <div>CoursePlayer</div>
+        <div style={{ paddingTop: '41%', position: 'relative' }}>
+            {videoData.otp && videoData.playbackInfo !== "" && (
+                <iframe
+                    src={`https://player.vdocipher.com/v2/?otp=${videoData.otp}&playbackInfo=${videoData.playbackInfo}&player=YWu8FLUqyfqqwzPw`}
+                    style={{
+                        border: 0,
+                        width: "98%",
+                        height: "100%",
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                    }}
+                    allowFullScreen={true}
+                    allow="encrypted-media"
+                />
+            )}
+        </div>
     )
 }
 export default CoursePlayer
