e7b9626a5b95074802bddb0a3955fe32171498f3
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-12-20 21:22:52 +0600

    Complete manage-team and all user page

--- client/app/components/Admin/Users/AllUsers.tsx
+++ client/app/components/Admin/Users/AllUsers.tsx
@@ -1,22 +1,71 @@
 'use client'
 import { DataGrid } from '@mui/x-data-grid';
-import { Box, Button } from '@mui/material';
+import { Box, Button, Modal } from '@mui/material';
 import { AiOutlineDelete, AiOutlineMail } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti'
 import { useTheme } from 'next-themes';
 import { format } from 'timeago.js'
 
 import Loader from '../../Loader/Loader';
-import { useGetAllUsersQuery } from '../../../../redux/features/user/userApi';
-import { FC, useState } from 'react';
+import { useDeleteUserMutation, useGetAllUsersQuery, useUpdateUserRoleMutation } from '../../../../redux/features/user/userApi';
+import { FC, useEffect, useState } from 'react';
 import { styles } from '@/app/styles/style';
+import { useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
 
 type Props = {
     isTeam: boolean;
 }
 const AllUsers: FC<Props> = ({ isTeam }) => {
+    const { user } = useSelector((state: any) => state.auth);
     const [active, setActive] = useState(false)
+    const [open, setOpen] = useState(false);
+    const [userId, setUserId] = useState("");
+    const [email, setEmail] = useState("");
+    const [role, setRole] = useState("admin");
     const { theme, setTheme } = useTheme();
-    const { isLoading, data, error } = useGetAllUsersQuery({});
+    const { isLoading, data, refetch } = useGetAllUsersQuery({}, { refetchOnMountOrArgChange: true });
+    const [updateUserRole, { error: updateError, isSuccess }] = useUpdateUserRoleMutation();
+    const [deleteUser, { error: deleteError, isSuccess: deleteSuccess }] = useDeleteUserMutation({});
+
+
+    const handleDelete = async () => {
+        const id = userId;
+        await deleteUser(id);
+    };
+    const handleSubmit = async () => {
+        await updateUserRole({ email, role });
+    };
+
+    useEffect(() => {
+        if (updateError) {
+            if ("data" in updateError) {
+                const errorMessage = updateError as any;
+                toast.error(errorMessage.data.message);
+            }
+        }
+
+        if (isSuccess) {
+            refetch();
+            toast.success("User role updated successfully");
+            setActive(false);
+            setEmail("");
+            setRole("admin");
+        }
+        if (deleteSuccess) {
+            refetch();
+            toast.success("Delete user successfully!");
+            setOpen(false);
+            setUserId("");
+        }
+        if (deleteError) {
+            if ("data" in deleteError) {
+                const errorMessage = deleteError as any;
+                toast.error(errorMessage.data.message);
+            }
+        }
+    }, [updateError, isSuccess, deleteSuccess, deleteError]);
+
 
     const columns = [
         { field: "id", headerName: "ID", flex: 0.3 },
@@ -30,13 +79,27 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
             headerName: "Delete",
             flex: 0.2,
             renderCell: (params: any) => {
+                const isCurrentUser = user._id === params.row.id;
+
                 return (
                     <>
-                        <Button>
-                            <AiOutlineDelete
-                                className='dark:text-white text-black'
-                                size={20}
-                            />
+                        <Button onClick={() => {
+                            setOpen(!open);
+                            setUserId(params.row.id);
+                        }} disabled={isCurrentUser}>
+                            {
+                                isCurrentUser ? (
+                                    <TiDeleteOutline
+                                        className={`dark:text-white text-black`}
+                                        size={20}
+                                    />
+                                ) : (
+                                    <AiOutlineDelete
+                                        className={`dark:text-white text-black`}
+                                        size={20}
+                                    />
+                                )
+                            }
                         </Button>
                     </>
                 )
@@ -67,7 +130,7 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
 
     const rows: any = []
 
-    if (isTeam) {
+    if (isTeam && user.role === 'admin') {
         const newData = data && data.users.filter((item: any) => item.role === 'admin');
         newData && newData.forEach((item: any) => {
             rows.push({
@@ -102,13 +165,15 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
                         {
                             isTeam &&
                             (
-                                <div className="w-full flex justify-end">
+                                <div
+                                    className="w-full flex justify-end"
+                                    onClick={() => setActive(!active)}
+                                >
                                     {/* <div className={`${styles.button} !h-[35px] !w-[220px] !bg-[#57c7a3] !font-[400]`}>
                                 Add New Member
                             </div> */}
                                     <div
                                         className={`w-[200px] h-[40px] bg-transparent border border-[#37a39a] dark:text-[#fff] text-black rounded-[3px] flex justify-center items-center cursor-pointer hover:bg-[#37a39a] hover:text-white`}
-                                        onClick={() => setActive(!active)}
                                     >
                                         Add New Member
                                     </div>
@@ -184,11 +249,99 @@ const AllUsers: FC<Props> = ({ isTeam }) => {
                                 columns={columns}
                             />
                         </Box>
+                        {active && (
+                            <Modal
+                                open={active}
+                                onClose={() => {
+                                    setActive(!active);
+                                    setEmail("");
+                                    setRole("admin");
+                                }}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                                disableScrollLock
+                                sx={{
+                                    "&.MuiModal-root": {
+                                        zIndex: "2147483647!important",
+                                    },
+                                }}
+                            >
+                                <Box className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
+                                    <h1 className={`${styles.title}`}>Add New Member</h1>
+                                    <div className="mt-4">
+                                        <input
+                                            type="email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            placeholder="Enter email..."
+                                            className={`${styles.input}`}
+                                        />
+                                        <select
+                                            name=""
+                                            id=""
+                                            className={`${styles.input} !mt-6`}
+                                            onChange={(e: any) => setRole(e.target.value)}
+                                        >
+                                            <option
+                                                className="dark:bg-[#000] text-[#fff]"
+                                                value="admin"
+                                            >
+                                                Admin
+                                            </option>
+                                            <option className="dark:bg-[#000] text-[#fff]" value="user">
+                                                User
+                                            </option>
+                                        </select>
+                                        <br />
+                                        <div
+                                            className={`${styles.button} my-6 !h-[30px] cursor-pointer`}
+                                            onClick={handleSubmit}
+                                        >
+                                            Submit
+                                        </div>
+                                    </div>
+                                </Box>
+                            </Modal>
+                        )}
+                        {open && (
+                            <Modal
+                                open={open}
+                                onClose={() => { setOpen(!open); setUserId(""); }}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                                disableScrollLock
+                                sx={{
+                                    "&.MuiModal-root": {
+                                        zIndex: "2147483647!important",
+                                    },
+                                }}
+                            >
+                                <Box className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
+                                    <h1 className={`${styles.title}`}>
+                                        Are you sure you want to delete this user?
+                                    </h1>
+                                    <div className="flex w-full items-center justify-between mb-6 mt-4">
+                                        <div
+                                            className={`${styles.button} !w-[120px] h-[30px] bg-[#57c7a3] cursor-pointer`}
+                                            onClick={() => setOpen(!open)}
+                                        >
+                                            Cancel
+                                        </div>
+                                        <div
+                                            className={`${styles.button} !w-[120px] h-[30px] bg-[#d63f3f] cursor-pointer`}
+                                            onClick={handleDelete}
+                                        >
+                                            Delete
+                                        </div>
+                                    </div>
+                                </Box>
+                            </Modal>
+                        )}
                     </Box>
                 )
             }
-
         </div>
     )
 }
+
 export default AllUsers
