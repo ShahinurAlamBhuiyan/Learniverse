04b7665e14f7949dd1c457ed4b59116fe61fde88
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-01-10 23:12:30 +0600

    warning prompt change

--- client/app/components/Admin/Course/AllCourses.tsx
+++ client/app/components/Admin/Course/AllCourses.tsx
@@ -198,7 +198,7 @@ const AllCourses = (props: Props) => {
                                 >
                                     <Box className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
                                         <h1 className={`${styles.title}`}>
-                                            Are you sure you want to delete this course?
+                                            Are you sure you want to remove this course?
                                         </h1>
                                         <div className="flex w-full items-center justify-between mb-6 mt-4">
                                             <div
