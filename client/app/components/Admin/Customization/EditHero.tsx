a0137a0cd29865c2296e91e3e0651b086b230f2a
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-01-17 23:02:05 +0600

    Complete Hero customization section for admin

--- client/app/components/Admin/Customization/EditHero.tsx
+++ client/app/components/Admin/Customization/EditHero.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { styles } from '../../../../app/styles/style'
-import { useGetHeroDataQuery } from '../../../../redux/features/layout/layoutApi'
+import { useEditLayoutMutation, useGetHeroDataQuery } from '../../../../redux/features/layout/layoutApi'
 import React, { FC, useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import { AiOutlineCamera } from 'react-icons/ai'
@@ -13,47 +13,47 @@ const EditHero: FC<Props> = ({ }) => {
     const [title, setTitle] = useState('')
     const [subTitle, setSubTitle] = useState('')
     const { data, refetch } = useGetHeroDataQuery("Banner", { refetchOnMountOrArgChange: true })
-    //   const [editLayouts, { isLoading, isSuccess, error }] = useEditLayoutsMutation()
+    const [editLayouts, { isLoading, isSuccess, error }] = useEditLayoutMutation()
     useEffect(() => {
         if (data) {
-            setImage(data?.data?.banner?.image?.url)
-            setTitle(data?.data?.banner?.title)
-            setSubTitle(data?.data?.banner?.subtitle)
+            setTitle(data?.layout?.banner.title);
+            setSubTitle(data?.layout?.banner.subTitle);
+            setImage(data?.layout?.banner?.image?.url);
         }
-    }, [data])
-    //   useEffect(() => {
-    //     if (isSuccess) {
-    //       refetch()
-    //       toast.success("Hero Updated successfully")
-    //     }
-    //     if (error) {
-    //       if ('data' in error) {
-    //         const errorData = error as any;
-    //         toast.error(errorData.data.message)
-    //       }
-    //     }
-    //   }, [isSuccess, error])
+        if (isSuccess) {
+            refetch()
+            toast.success("Hero Updated successfully")
+        }
+        if (error) {
+            if ('data' in error) {
+                const errorData = error as any;
+                toast.error(errorData.data.message)
+            }
+        }
+    }, [data, isSuccess, error])
+
     const handleEdit = async () => {
-        // await editLayouts({
-        //   type: "Banner",
-        //   banner: {
-        //     title,
-        //     subtitle: subTitle,
-        //     image,
-        //   }
-        // })
+        await editLayouts({
+            type: "Banner",
+            title,
+            subTitle,
+            image,
+        })
     }
     const handleUpdate = async (e: any) => {
-        // const file = e.target.files[0]
-        // if (file) {
-        //   const reader = new FileReader()
-        //   reader.onloadend = () => {
-        //     setImage(reader.result as string)
-        //   }
-        //   reader.readAsDataURL(file)
-        // }
+        const file = e.target.files?.[0]
+        if (file) {
+            const reader = new FileReader()
+            reader.onload = (e: any) => {
+                if (reader.readyState === 2) {
+                    setImage(e.target.result as string);
+                }
+            }
+            reader.readAsDataURL(file)
+        }
 
     }
+
     return (
         <>
             <div className="w-full 1000px:flex items-center">
@@ -98,27 +98,23 @@ const EditHero: FC<Props> = ({ }) => {
                     <br />
                     <div
                         className={`${styles.button
-                            } !w-[100px] !min-h-[40px] !h-[40px] dark:text-white text-black bg-[#cccccc34] 
-          ${data?.data?.banner?.title !== title ||
-                                data?.data?.banner?.subtitle !== subTitle ||
-                                data?.data?.banner?.image?.url !== image
+                            } !w-[100px] !min-h-[40px] !h-[40px] dark:text-white text-black bg-[#cccccc34]  
+          ${data?.layout?.banner?.title !== title ||
+                                data?.layout?.banner?.subTitle !== subTitle ||
+                                data?.layout?.banner?.image?.url !== image
                                 ? "!cursor-pointer !bg-[#42d383]"
                                 : "!cursor-not-allowed"
                             }
           !rounded absolute bottom-12 right-12`}
                         onClick={
-                            data?.data?.banner?.title !== title ||
-                                data?.data?.banner?.subtitle !== subTitle ||
-                                data?.data?.banner?.image?.url !== image
+                            data?.layout?.banner?.title !== title ||
+                                data?.layout?.banner?.subTitle !== subTitle ||
+                                data?.layout?.banner?.image?.url !== image
                                 ? handleEdit
                                 : () => null
                         }
                     >
-                        {/* {isLoading ? (
-              <ImSpinner2 className="animate-spin text-white" size={24} />
-            ) : ( */}
-                        "Save"
-                        {/* )} */}
+                        Save
                     </div>
                 </div>
             </div>
