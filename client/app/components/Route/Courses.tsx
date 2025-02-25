16b0a0bc539a582fd44ff8f04c4013fbd020734e
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2025-02-26 01:14:13 +0600

    Implemented all courses in homepage

--- client/app/components/Route/Courses.tsx
+++ client/app/components/Route/Courses.tsx
@@ -1,10 +1,12 @@
 import { useGetUsersAllCoursesQuery } from "@/redux/features/courses/coursesApi"
 import { FC, useEffect, useState } from "react"
+import CourseCard from '../CourseCard/CourseCard'
 
 type Props = {}
 const Courses: FC<Props> = () => {
     const { data, isLoading } = useGetUsersAllCoursesQuery({});
     const [courses, setCourses] = useState<any[]>([]);
+    const [route, setRoute] = useState<any[]>([]);
 
     useEffect(() => {
         setCourses(data?.courses)
@@ -18,6 +20,18 @@ const Courses: FC<Props> = () => {
                     <span className="dark:text-[#46e256] text-[crimson]">Opportunity</span> <br />
                     Opportunity With Our Courses
                 </h1>
+                <br /><br />
+                <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] 1500px:grid-cols-4 1500px:gap-[35px] mb-12 border-0">
+                    {courses &&
+                        courses.map((item: any, index: number) => (
+                            <CourseCard
+                                item={item}
+                                key={index}
+                            />
+                        ))
+
+                    }
+                </div>
             </div>
         </div>
     )
